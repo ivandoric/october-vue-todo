c4edfff1d0cac597da8a8d3decfa35f7a81bf820
Ivan Dorić <idoric1@gmail.com>
2018-09-23 10:59:36 +0200

    Commit message

--- vue-todo/src/components/Hello.vue
+++ vue-todo/src/components/Hello.vue
@@ -7,7 +7,7 @@
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://router.vuejs.org/" target="_blank">Vue router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
